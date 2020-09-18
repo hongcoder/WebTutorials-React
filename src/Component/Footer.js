7a71109e280e843a1bd7838035399c862cace5ea
hongcoder <honghs93@gmail.com>
2020-09-18 22:03:57 +0900

    footer change again

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,6 @@ function Footer(){
   return (
     <footer className='bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full'>
       &copy; Copyright 2020
-      this is footer
     </footer>
   )
 }
